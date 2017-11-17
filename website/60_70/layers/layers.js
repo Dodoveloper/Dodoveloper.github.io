030a0d68c66777d460f3ce6a00e314fd8305fca6
DodoIta <davide.cor94@gmail.com>
2017-11-17 16:35:12 +0100

    uploaded website

---
+++ website/60_70/layers/layers.js
@@ -0,0 +1,42 @@
+var wms_layers = [];
+var baseLayer = new ol.layer.Group({
+    'title': '',
+    layers: [
+new ol.layer.Tile({
+    'title': 'OSM',
+    'type': 'base',
+    source: new ol.source.OSM()
+})
+]
+});
+var format_Meteorite_Landings_0 = new ol.format.GeoJSON();
+var features_Meteorite_Landings_0 = format_Meteorite_Landings_0.readFeatures(json_Meteorite_Landings_0, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Meteorite_Landings_0 = new ol.source.Vector({
+    attributions: [new ol.Attribution({html: '<a href=""></a>'})],
+});
+jsonSource_Meteorite_Landings_0.addFeatures(features_Meteorite_Landings_0);var lyr_Meteorite_Landings_0 = new ol.layer.Vector({
+                source:jsonSource_Meteorite_Landings_0, 
+                style: style_Meteorite_Landings_0,
+    title: 'Meteorite_Landings<br />\
+    <img src="styles/legend/Meteorite_Landings_0_0.png" />  0.00 - 0.12 <br />\
+    <img src="styles/legend/Meteorite_Landings_0_1.png" />  0.12 - 0.93 <br />\
+    <img src="styles/legend/Meteorite_Landings_0_2.png" />  0.93 - 3.85 <br />\
+    <img src="styles/legend/Meteorite_Landings_0_3.png" />  3.85 - 13.00 <br />\
+    <img src="styles/legend/Meteorite_Landings_0_4.png" />  13.00 - 3000.00 <br />'
+        });
+
+lyr_Meteorite_Landings_0.setVisible(true);
+var layersList = [baseLayer,lyr_Meteorite_Landings_0];
+lyr_Meteorite_Landings_0.set('fieldAliases', {'name': 'name', 'id': 'id', 'nametype': 'nametype', 'recclass': 'Class', 'mass (g)': 'mass (g)', 'fall': 'fall', 'year': 'Year', 'reclat': 'reclat', 'reclong': 'reclong', 'GeoLocation': 'GeoLocation', 'Weight (Kg)': 'Weight (Kg)', 'years': 'Year', });
+lyr_Meteorite_Landings_0.set('fieldImages', {'name': 'TextEdit', 'id': 'Hidden', 'nametype': 'Hidden', 'recclass': 'TextEdit', 'mass (g)': 'Hidden', 'fall': 'Hidden', 'year': 'Hidden', 'reclat': 'Hidden', 'reclong': 'Hidden', 'GeoLocation': 'Hidden', 'Weight (Kg)': 'TextEdit', 'years': 'TextEdit', });
+lyr_Meteorite_Landings_0.set('fieldLabels', {'name': 'inline label', 'recclass': 'inline label', 'Weight (Kg)': 'inline label', 'years': 'inline label', });
+lyr_Meteorite_Landings_0.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
+    lyr_Meteorite_Landings_0.on("postcompose", update);
+
+    var listenerKey = lyr_Meteorite_Landings_0.on('change', function(e) {
+        update();
+        ol.Observable.unByKey(listenerKey);
+    });
---
+++ website/90_00/styles/Meteorite_Landings_0_style.js
@@ -0,0 +1,102 @@
+var size = 0;
+
+
+var style_Meteorite_Landings_0 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("Weight (Kg)");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10.725px \'MS Shell Dlg 2\', sans-serif";
+    var labelFill = "rgba(0, 0, 0, 1)";
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value > 0.000000 && value <= 0.005100) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(255,255,178,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill)
+    })]
+                    } else if (value > 0.005100 && value <= 0.014000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(254,204,92,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill)
+    })]
+                    } else if (value > 0.014000 && value <= 0.041000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(253,141,60,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill)
+    })]
+                    } else if (value > 0.041000 && value <= 0.174000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(240,59,32,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill)
+    })]
+                    } else if (value > 0.174000 && value <= 2000.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(189,0,38,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill)
+    })]
+                    };
+
+    return style;
+}
+function update() {
+
+    var features = lyr_Meteorite_Landings_0.getSource().getFeatures();
+    features.forEach(function(feature){
+        var context = {
+            feature: feature,
+            variables: {}
+        };
+
+        // Get the label text as a string
+        var text = "";
+
+        // Get the center point in pixel space
+        var center = ol.extent.getCenter(feature.getGeometry().getExtent());
+        var pixelCenter = map.getPixelFromCoordinate(center);
+
+        var size = 12;
+        var halfText = (size + 1) * (text.length / 4);
+
+        // Create a bounding box for the label using known pixel heights
+        var minx = parseInt(pixelCenter[0] - halfText);
+        var maxx = parseInt(pixelCenter[0] + halfText);
+
+        var maxy = parseInt(pixelCenter[1] - (size / 2));
+        var miny = parseInt(pixelCenter[1] + (size / 2));
+
+        // Get bounding box points back into coordinate space
+        var min = map.getCoordinateFromPixel([minx, miny]);
+        var max = map.getCoordinateFromPixel([maxx, maxy]);
+
+        // Create the bounds
+        var bounds = {
+            bottomLeft: min,
+            topRight: max
+        };
+        // Weight longer labels higher, use their name as the ID
+        labelEngine.ingestLabel(bounds, text, text.length, feature)
+
+    });
+
+    // Call the label callbacks for showing and hiding
+    labelEngine.update();
+
+};
